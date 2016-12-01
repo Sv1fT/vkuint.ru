4ef649f08790a6c8a676d69171997e6ea144eb0f
Sv1fT <oson-dmitrij@yandex.ru>
2016-12-01 09:14:21 +0400

    First load from git

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -217,5 +217,6 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
+<script src="//cdn.ckeditor.com/4.6.0/full/ckeditor.js"></script>
 </body>
 </html>
